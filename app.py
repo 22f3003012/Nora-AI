ae7bfe5b8e56db6a3de5a4a8e23da3666ba58d6e
Ritwik Chandra <Ritwik1607@users.noreply.huggingface.co>
2025-02-11 23:32:07 +0000

    Update app.py

--- app.py
+++ app.py
@@ -126,8 +126,7 @@ with gr.Blocks(theme='gstaff/xkcd', css = css) as app:
 
     chatbot = gr.ChatInterface(
         fn=chatbot_response,
-        title="Chat with Nora 💬",
-        examples=[["Hey love! 💕 I'm so happy to see you here! What’s on your mind? 😊"]]
+        title="Chat with Nora 💬"
 
     )
 
