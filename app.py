8963e34e61b59c7b2c5a3400f68a17cde4ec981f
Ritwik Chandra <Ritwik1607@users.noreply.huggingface.co>
2025-02-11 23:21:24 +0000

    Update app.py

--- app.py
+++ app.py
@@ -126,7 +126,8 @@ with gr.Blocks(theme='gstaff/xkcd', css = css) as app:
 
     chatbot = gr.ChatInterface(
         fn=chatbot_response,
-        title="Chat with Nora 💬"
+        title="Chat with Nora 💬",
+        fill_height: True
     )
 
     gr.Markdown(
